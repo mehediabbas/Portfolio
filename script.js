116b60caec33478b4be98f246bdeff0babcf5eca
mabbasmehedi <mehedi2205101523@diu.edu.bd>
2025-12-04 23:33:06 +0600

    finalcommit

--- script.js
+++ script.js
@@ -24,4 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
         section.classList.add('hidden-section');
         sectionObserver.observe(section);
     });
+
+    const burger = document.querySelector('.burger-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li a');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('active');
+        });
+    });
 });
